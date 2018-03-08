c5e496925be8b6eb057f8020432102d8ae66c5b7
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-03-07 23:05:32 -0500

    added redux

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+// ROOT reducers
+
+
+import { combineReducers } from 'redux';
+import sessionReducer from './session';
+import userReducer from './user';
+
+
+const rootReducer = combineReducers({
+  sessionState: sessionReducer,
+  userState: userReducer,
+});
+
+export default rootReducer;
